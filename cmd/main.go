4fa2a0ee615cf3abaf1c3aac17238ecd07d36f2a
Fabio Meireles <fbomrl14@gmail.com>
2025-08-25 13:39:35 -0300

    Inclusão conexão com sqlserver

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/fbomrl/animon-go/internal/database"
+	"github.com/joho/godotenv"
 )
 
 var temp = template.Must(template.ParseGlob("../templates/*.html"))
 
 func main() {
+	// variaveis .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erro ao carregar .env")
+	}
+
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		log.Fatal("DSN não definido na variável DB_DSN")
+	}
+	db, err := database.SqlServer(dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
