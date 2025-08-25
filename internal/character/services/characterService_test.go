a93d800757b82c211d5f9f9b16334896eeaff1e9
Fabio Meireles <fbomrl14@gmail.com>
2025-08-25 17:13:11 -0300

    Inclusão de testes do characterById

--- internal/character/services/characterService_test.go
+++ internal/character/services/characterService_test.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/fbomrl/animon-go/internal/character/model"
@@ -12,13 +13,18 @@ type FakeRepository struct {
 }
 
 func (f *FakeRepository) CharacterById(id int) (*model.Character, error) {
-	return f.character, f.err
+
+	if f.character != nil && f.character.Id == id {
+		return f.character, f.err
+	}
+	return nil, fmt.Errorf("personagem não encontrado")
 }
 func (f *FakeRepository) FindAllCharacters() ([]*model.Character, error) {
 	return []*model.Character{f.character}, f.err
 }
 
 func TestCharacterByIdService(t *testing.T) {
+	//PERSONAGEM EXISTE
 	repo := &FakeRepository{
 		character: &model.Character{
 			Id:   1,
@@ -28,12 +34,20 @@ func TestCharacterByIdService(t *testing.T) {
 	}
 	characterService := &CharacterService{RepoCharacter: repo}
 	character, err := characterService.CharacterByIdService(1)
-
 	if err != nil {
 		t.Errorf("Teste: esperado sem erro - recebido: %v", err)
 		return
 	}
 	if character == nil {
 		t.Errorf("Teste: esperado personagem não nulo - recebido: nil")
+	}
+
+	//PERSONAGEM NÃO EXISTE
+	character, err = characterService.CharacterByIdService(-1)
+	if err == nil {
+		t.Errorf("Teste: esperado erro - recebido: nil")
+	}
+	if character != nil {
+		t.Errorf("Teste: esperado nil - recebido: %v", character)
 	}
 }
